b39eb0885d5bfb4897bbe23fab78b67d57a86bd0
wlagyorgy <wlagyorgy@gmail.com>
2017-12-10 18:08:47 +0100

    stats

--- app/src/main/java/hu/bme/wlassits/budget/presentation/statistics/IncomeStatisticsActivity.java
+++ app/src/main/java/hu/bme/wlassits/budget/presentation/statistics/IncomeStatisticsActivity.java
@@ -5,4 +5,5 @@ package hu.bme.wlassits.budget.presentation.statistics;
  */
 
 public class IncomeStatisticsActivity {
+
 }
--- app/src/main/java/hu/bme/wlassits/budget/presentation/statistics/StatisticsActivity.java
+++ app/src/main/java/hu/bme/wlassits/budget/presentation/statistics/StatisticsActivity.java
@@ -1,7 +1,10 @@
 package hu.bme.wlassits.budget.presentation.statistics;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import hu.bme.wlassits.budget.R;
@@ -17,21 +20,39 @@ public class StatisticsActivity extends BaseActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistics);
+        Button outlaysSumBtn = findViewById(R.id.btnOutlaysSum);
+        Button incomesSumBtn = findViewById(R.id.btnIncomesSum);
         long outlaysSum = 0;
         long incomesSum = 0;
         int i;
-        for(i = 0; i< outlays.size(); i++){
+        for (i = 0; i < outlays.size(); i++) {
             outlaysSum += outlays.get(i).getValue();
         }
 
-        for(i = 0; i< incomes.size(); i++){
+        for (i = 0; i < incomes.size(); i++) {
             incomesSum += incomes.get(i).getValue();
         }
 
-        TextView tvOut = findViewById(R.id.tvOutlayStats);
-        TextView tvIn = findViewById(R.id.tvIncomeStats);
-        tvOut.setText(String.valueOf(outlaysSum));
-        tvIn.setText(String.valueOf(incomesSum));
+
+        outlaysSumBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getBaseContext(), OutlaysStatisticsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        incomesSumBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                    Intent intent = new Intent(getBaseContext(), IncomeStatisticsActivity.class);
+                    startActivity(intent);
+                }
+
+            });
+
+
 
         //TODO Gyuri 7. Egyszerű statisztikák hozzáadása, pl összes bevétel és kiadás az első login óta
     }
